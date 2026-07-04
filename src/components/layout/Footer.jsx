d5bd04e32dc4620aa3912982a3f6efbdf6264154
deepaksharmapw3-blip <deepaksharmapw3@gmail.com>
2026-07-04 13:35:05 +0530

    Initial project structure

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#0f172a",
+        color: "white",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      © 2026 TechSphere | Powered by GeeksforGeeks Student Chapter
+    </footer>
+  );
+}
